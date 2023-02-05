ce80ba9af71a02492a433f27bdd48c6f76a8f3df
swemaster <64152321+swemaster@users.noreply.github.com>
2023-02-05 06:45:18 -0500

    table + scores

--- public/team.js
+++ public/team.js
@@ -9,11 +9,13 @@ fetch('./teams.json')
     json.forEach(element => {
         let row = table.insertRow(-1);
         let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
         cell0.innerHTML = element.name
         element.members.forEach(mate=> {
             let cell = row.insertCell(i);
             cell.innerHTML = mate;
             i++;
+        cell1.innerHTML = element.score
         });
         i = 1
     });
